305c6b93fdbe8fc4a489b317166f2c7283af8b6b
elyoguictm <gabyferal@gmail.com>
2021-02-03 17:02:16 +0000

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - main funtion
  *
  * Return: 0 to exit
  */
-
 int main(void)
 {
 printf("Size of a char:%d byte(s)\n", sizeof(char));
@@ -13,6 +11,5 @@ printf("Size of an int:%d byte(s)\n", sizeof(int));
 printf("Size of a long int:%d byte(s)\n", sizeof(long int);
 printf("Size of a long long int:%d byte(s)\n", sizeof(long long int);
 printf("Size of a float:%d byte(s)\n", sizeof(float);
-
 return (0);
 }
