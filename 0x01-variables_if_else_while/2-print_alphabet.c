63b91efa5cb6996a9f8353923786c71b0171346f
elyoguictm <gabyferal@gmail.com>
2021-02-04 17:28:31 +0000

    ifelse

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 /* more headers goes there */
-/* main - main funtion
-/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = a;
+
+	while (ch <="z")
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar("\n");
+
+	return (0);
+}
