cb54ea91a9f3304548180ba41e871749d58c826f
elyoguictm <gabyferal@gmail.com>
2021-02-04 16:19:23 +0000

    ifelse

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,16 +15,18 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	{
-		printf("d% is positive\n"), n;
-	}
-	else if (n == 0)
-	{
-		printf("d% is zero\n"), n;
-	}
-	else if (n < 0)
-	{
-		printf("is negative\n");
-	}
+
+		printf("d% is positive\n", n);
+
+	else
+		if (n == 0)
+
+			printf("d% is zero\n", n);
+
+	else
+		if (n < 0)
+
+			printf("is negative\n", n);
+
 	return (0);
 }
