788652e598f610ebab4f41892fb8af6dd9eea5bd
elyoguictm <gabyferal@gmail.com>
2021-02-09 14:15:42 +0000

    loop

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,35 @@
 #include "holberton.h"
 
 /**
- * times_table - lalalalaa
+ * times_table - prints the table
  *
- * Return: dkalkdslkda
- *
+ * Return: the last digit of the number
  */
 
+void times_table(void)
+{
+	int z, x;
+
+	for (z = 0; z < 10; z++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			if ((z * x) >= 10)
+			{
+				_putchar((z * x) / 10 + '0');
+				_putchar((z * x) % 10 + '0');
+			}
+			else
+			{
+				if (x != 0)
+					_putchar(' ');
+				_putchar((z * x) + '0');
+			}
+			if (x == 9)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
