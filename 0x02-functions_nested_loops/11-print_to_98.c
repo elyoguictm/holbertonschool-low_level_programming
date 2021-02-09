2ed6a684004ac53cbcc684d33baa3207ce7a8180
elyoguictm <gabyferal@gmail.com>
2021-02-09 15:45:16 +0000

    vro

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98,
- * followed by a new line
- * @n: print from this number
+ * print_to_98 - prints 
+ * @n: fdsfsd
  */
 
 void print_to_98(int n)
