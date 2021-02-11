8748b6607ea4850984317c47e0e38492ef16a891
elyoguictm <gabyferal@gmail.com>
2021-02-11 16:32:14 +0000

    xdd

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - print
+ */
+
+void more_numbers(void)
+{
+	int num;
+	int times;
+
+	for (times = 0; times < 10; times++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
