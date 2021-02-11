9a0c219c42947ccb8376e6684a66246d1e582fdb
elyoguictm <gabyferal@gmail.com>
2021-02-11 12:50:49 +0000

    add

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * main - print
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int i, t1 = 1, t2 = 2, nextTerm;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (i != 50)
+			printf("%ld, ", t1);
+		else
+			printf("%ld", t1);
+		nextTerm = t1 + t2;
+		t1 = t2;
+		t2 = nextTerm;
+	}
+	printf("\n");
+	return (0);
+}
