df82934335cfd67860bd60e9fb43771dcf55b03b
elyoguictm <gabyferal@gmail.com>
2021-02-16 22:33:51 +0000

    uwu

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_array - print array
+ * @a: pointer to the array
+ * @n: number of elements
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int z;
+
+	for (z = 0; z < z; z++)
+	{
+		if (z == 0)
+		{
+			printf("%d", a[z]);
+		}
+		else
+		{
+			printf(", %d", a[z]);
+		}
+	}
+	putchar('\n');
+}
