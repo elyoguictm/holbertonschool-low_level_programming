36073edf44c671d2be32818c3cb5cd0c4c695519
elyoguictm <gabyferal@gmail.com>
2021-02-16 19:44:48 +0000

    aa

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *print_rev - print rev
+ *
+ *@s: ssasa
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		_putchar(s[i - 1]);
+		i--;
+	}
+	_putchar('\n');
+}
