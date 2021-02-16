ff676cd567620ec12a14575aad8a58f5a35da17b
elyoguictm <gabyferal@gmail.com>
2021-02-16 19:58:50 +0000

    d

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * puts2 - puts2
+ * @str: str sdads
+ */
+void puts2(char *str)
+{
+	int i = 0, len = 0;
+
+	while (str[i++])
+		len++;
+
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
