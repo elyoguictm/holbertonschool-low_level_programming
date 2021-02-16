699bd7437a1d27ca1d98b2da8728a4740fc89081
elyoguictm <gabyferal@gmail.com>
2021-02-16 00:21:44 +0000

    olo

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * swap_int - Entry point
+ * @a: no tengo imaginacion
+ * @b: no tengo imaginacion x2
+ * Description: no quiero :c
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
