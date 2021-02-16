c6464de7e04a031a5ac7574facce6235177b86ff
elyoguictm <gabyferal@gmail.com>
2021-02-16 00:10:45 +0000

    new

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Int n 
+ * @n: es una n
+ *
+ * Description: muchisimo texto para tan poco code
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
