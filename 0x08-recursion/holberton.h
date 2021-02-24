ced23742772b9c4b5ee108cf97c7e278dc9208a3
gabo <gabyferal@gmail.com>
2021-02-24 20:56:44 +0000

    a

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,4 +1,4 @@
-void _puts_recursion(char *s);
+Bvoid _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -7,3 +7,4 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int square_root(int z, int x)
