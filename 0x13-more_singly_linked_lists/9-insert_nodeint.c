cbc97f4dc6d1cc5d1e2a351f42dc611a0013559b
elyoguictm <gabyferal@gmail.com>
2021-03-23 22:40:59 +0000

    a

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *aux = *head;
+	listint_t *new, *in = *head;
 	unsigned int z = 0;
 
 	while (in && z < idx - 1)
